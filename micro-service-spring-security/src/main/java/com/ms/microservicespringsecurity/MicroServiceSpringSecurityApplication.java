36b0679e9cd647cb383bacfd885dfbb58e143af0
dellamas <80655200+dellamas@users.noreply.github.com>
2023-03-18 16:15:53 -0300

    add files

---
+++ micro-service-spring-security/src/main/java/com/ms/microservicespringsecurity/MicroServiceSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ms.microservicespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ micro-service-spring-security/src/test/java/com/ms/microservicespringsecurity/MicroServiceSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.microservicespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
