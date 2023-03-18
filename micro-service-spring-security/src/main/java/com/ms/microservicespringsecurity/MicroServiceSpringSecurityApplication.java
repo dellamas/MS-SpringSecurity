31a85af26dd8469912630eecddff1c8ee25d8bdd
dellamas <luisfabricio@hotmail.com.br>
2023-03-18 19:48:01 -0300

    -Criação de uma rota pública e uma privada com autentiação.(testes iniciais)

--- micro-service-spring-security/src/main/java/com/ms/microservicespringsecurity/MicroServiceSpringSecurityApplication.java
+++ micro-service-spring-security/src/main/java/com/ms/microservicespringsecurity/MicroServiceSpringSecurityApplication.java
@@ -2,6 +2,8 @@ package com.ms.microservicespringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MicroServiceSpringSecurityApplication {
@@ -9,5 +11,19 @@ public class MicroServiceSpringSecurityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceSpringSecurityApplication.class, args);
 	}
+	@RestController
+	class httpController{
+		@GetMapping("/public")
+		String publicRoute(){
+			return "<h1>rota publica liberada </h1>";
+		}
+		@GetMapping("/private")
+		String privateRoute(){
+			return "<h1>rota privada liberada </h1>";
+		}
+
+
+	}
+
 
 }
