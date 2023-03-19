c012522090a9ac5949b930b535554efc7bbb00e3
dellamas <luisfabricio@hotmail.com.br>
2023-03-19 01:49:40 -0300

    Trabalhando com cookie e jwt, criando chave da api no google.

--- micro-service-spring-security/src/main/java/com/ms/microservicespringsecurity/MicroServiceSpringSecurityApplication.java
+++ micro-service-spring-security/src/main/java/com/ms/microservicespringsecurity/MicroServiceSpringSecurityApplication.java
@@ -2,6 +2,8 @@ package com.ms.microservicespringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,27 @@ public class MicroServiceSpringSecurityApplication {
 			return "<h1>rota publica liberada </h1>";
 		}
 		@GetMapping("/private")
-		String privateRoute(){
+		String privateRoute(@AuthenticationPrincipal OidcUser principal){
 			return "<h1>rota privada liberada </h1>";
+		}
+
+		@GetMapping("/cookie")
+		String cookie(@AuthenticationPrincipal OidcUser principal) {
+			return String.format("""
+     				<h1>rota privada OAuth2</h1>
+					<h3>Principal: %s</h3>
+					<h3>Email attribute: %s</h3>
+					<h3>Authorization: %s</h3>
+					<h3>JWT: %s</h3>
+							""",principal, principal.getAttribute("email"),
+							principal.getAuthorities(),principal.getIdToken().getTokenValue());
+		}
+
+		@GetMapping("/jwt")
+		String jwt(){
+			return String.format("""
+					<h1> JWT</h1>
+					""");
 		}
 
 
